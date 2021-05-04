7719d8c26d2b85f8d2f44da9ebb782de9bf82b51
Sergey <sgrechnev@gmail.com>
2021-05-04 15:59:09 +0700

    Minor changes
    - add a common path with different redirects for different roles
    - add exception handler and logger

--- project-TaskManagementSystem/src/main/java/ru/otus/controller/WebController.java
+++ project-TaskManagementSystem/src/main/java/ru/otus/controller/WebController.java
@@ -1,9 +1,15 @@
 package ru.otus.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.view.RedirectView;
 import ru.otus.model.Role;
 import ru.otus.service.AuthenticatedUserInfoService;
 import ru.otus.service.TaskService;
@@ -14,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 @CrossOrigin
 @Controller
 public class WebController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
 
     private final UserService userService;
 
@@ -27,7 +35,16 @@ public class WebController {
         this.authenticatedUserInfoService = authenticatedUserInfoService;
     }
 
-    @GetMapping({"/", "/tasks"})
+    @GetMapping("/")
+    public RedirectView mainPage() {
+        var user = authenticatedUserInfoService.getAuthenticatedUser();
+        if (user.getRole() == Role.ROLE_ADMIN) {
+            return new RedirectView("/users", true);
+        }
+        return new RedirectView("/tasks", true);
+    }
+
+    @GetMapping("/tasks")
     public String tasksView(Model model, final HttpServletResponse response) {
         model.addAttribute("tasks", taskService.getAuthorized());
         model.addAttribute("ROLE_MANAGER", Role.ROLE_MANAGER);
@@ -45,5 +62,12 @@ public class WebController {
 
     private void addUserInfo(Model model) {
         model.addAttribute("me", authenticatedUserInfoService.getAuthenticatedUser());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    private String handleMessageException(Exception e) {
+        logger.error("Exception: {}", e.getMessage());
+        return "Oops! Something went wrong. Error: " + e.getClass().getSimpleName();
     }
 }
