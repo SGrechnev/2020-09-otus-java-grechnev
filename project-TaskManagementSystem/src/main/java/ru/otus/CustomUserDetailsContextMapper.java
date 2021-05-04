0253971caaac5a957a6d6e595a1424609b52c039
Sergey <sgrechnev@gmail.com>
2021-05-04 16:06:32 +0700

    Improve authorization
    - add CustomUserDetailsContextMapper to enrich LDAPUser with Role
      from local DB
    - add authorization on some paths

---
+++ project-TaskManagementSystem/src/main/java/ru/otus/CustomUserDetailsContextMapper.java
@@ -0,0 +1,41 @@
+package ru.otus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ldap.core.DirContextAdapter;
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsMapper;
+import org.springframework.stereotype.Component;
+import ru.otus.model.Role;
+import ru.otus.service.UserService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class CustomUserDetailsContextMapper extends LdapUserDetailsMapper {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsContextMapper.class);
+
+    private final UserService userService;
+
+    public CustomUserDetailsContextMapper(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails mapUserFromContext(DirContextOperations ctx, String username, Collection<? extends GrantedAuthority> authorities) {
+        var user = userService.findByUsername(username);
+        Collection<Role> localAuthorities = new ArrayList<>();
+        user.ifPresent(value -> localAuthorities.add(value.getRole()));
+        return super.mapUserFromContext(ctx, username, localAuthorities);
+    }
+
+    @Override
+    public void mapUserToContext(UserDetails user, DirContextAdapter ctx) {
+        logger.info("mapUserFromContext(user: {}, ctx: {})", user, ctx);
+        super.mapUserToContext(user, ctx);
+    }
+}
--- project-TaskManagementSystem/src/main/java/ru/otus/WebSecurityConfig.java
+++ project-TaskManagementSystem/src/main/java/ru/otus/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package ru.otus;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,16 +27,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${ldap.enabled}")
     private String ldapEnabled;
 
+    @Autowired
+    private CustomUserDetailsContextMapper customUserDetailsContextMapper;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/users", "/api/users").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/tasks").hasRole("MANAGER")
+                .antMatchers(HttpMethod.POST, "/api/reports").hasRole("PERFORMER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).deleteCookies("JSESSIONID").permitAll()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .deleteCookies("JSESSIONID")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true).permitAll()
         ;
     }
 
@@ -50,7 +62,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .userDnPatterns(ldapUserDnPattern)
                 .passwordCompare()
 //                .passwordEncoder(new BCryptPasswordEncoder())
-                .passwordAttribute("userPassword");
+                .passwordAttribute("userPassword")
+                .and()
+                .userDetailsContextMapper(customUserDetailsContextMapper);
     }
 
 }
