ca85fa669adc78c7d9c62c04f84235b70971b4da
Sergey <sgrechnev@gmail.com>
2021-05-19 23:01:15 +0700

    minor changes
    - add report via js
    - refactor app.js (selectors)
    - update bootstrap (4.4.1-1 => 4.6.0)

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,5 +2,5 @@ public interface Versions {
     String guava = "28.2-jre";
     String junit_jupiter = "5.4.2";
     String jquery = "3.1.1-1";
-    String bootstrap = "4.4.1-1";
+    String bootstrap = "4.6.0";
 }
--- project-TaskManagementSystem/src/main/resources/static/app.js
+++ project-TaskManagementSystem/src/main/resources/static/app.js
@@ -17,7 +17,7 @@ function addReport(taskId) {
   const progressContainer = document.getElementById('add-report_progress_' + taskId);
   const commentContainer = document.getElementById('add-report_comment_' + taskId);
 
-  postData('/api/reports/', {taskId: taskId, progress: progressContainer.value, comment: commentContainer.value})
+  sendRequest('POST', '/api/reports/', {taskId: taskId, progress: progressContainer.value, comment: commentContainer.value})
     .then(
       response => {
         console.log("response: ", response);
@@ -37,61 +37,57 @@ function fail(response) {
 }
 
 function success(taskId, response) {
-  const errorContainer = document.getElementById('apiErrorMessage');
-  errorContainer.innerText = "";
-
-  const progressContainer = document.getElementById('add-report_progress_' + taskId);
-  const commentContainer = document.getElementById('add-report_comment_' + taskId);
-  const brElem = document.getElementById('task_reports_content_' + taskId).getElementsByTagName('br')[0];
-  const taskProgressValue = document.getElementById('task_progress_value_' + taskId);
-  const taskProgressBar = document.getElementById('task_progress_bar_' + taskId);
+  $('#apiErrorMessage').text("");
 
   // add report
   let div = document.createElement('div');
-  div.className = "card card-body";
-  div.innerText = JSON.stringify(response);
-  brElem.before(div);
+  div.innerHTML =
+      '<small id="creation-date" class="text-muted">creationDate</small>' +
+      '<div id="progress">progress</div>' +
+      '<div id="comment">comment</div>';
+  div.id = 'task_reports_content_' + response.taskId + '_' + response.id;
+  div.className = 'card card-body mt-2';
+  div.querySelector('#creation-date').innerText = response.creationDate;
+  div.querySelector('#progress').innerText = response.progress + '%';
+  div.querySelector('#comment').innerText = response.comment;
+  console.log("div:", div);
+  $('#task_reports_content_' + taskId +'>br').before(div);
   // set progress
-  taskProgressValue.innerText = response.progress + "%";
-  taskProgressBar.style.width = response.progress + "%";
+  $('#task_progress_value_' + taskId).text(response.progress + '%');
+  const taskProgressBar = $('#task_progress_bar_' + taskId);
+  taskProgressBar.width(response.progress + '%');
   if(response.progress == 100){
-    taskProgressBar.className = taskProgressBar.className.replaceAll("bg-warning", "bg-success");
+    taskProgressBar.addClass('bg-success').removeClass('bg-warning');
   } else {
-    taskProgressBar.className = taskProgressBar.className.replaceAll("bg-success", "bg-warning");
+    taskProgressBar.addClass('bg-warning').removeClass('bg-success');
   }
   // clear inputs
-  progressContainer.value = "";
-  commentContainer.value = "";
+  $('#add-report_progress_' + taskId).val("");
+  $('#add-report_comment_' + taskId).val("");
 }
 
 function addTask(creatorId) {
   console.log("creatorId: ", creatorId)
-  const performerId = document.getElementById('add-task_performer');
-  const description = document.getElementById('add-task_description');
-  const expectedResult = document.getElementById('add-task_expected_result');
-  const expectedDueDate = document.getElementById('add-task_expected_due_date');
 
-  const errorContainer = document.getElementById('apiErrorMessage');
-
-  postData('/api/tasks/', {performerId: performerId.value, description: description.value, expectedResult: expectedResult.value, expectedDueDate: expectedDueDate.value})
-    .then(
-      response => {
-        console.log("addTask::response:", response);
-        let div = document.createElement('div');
-        div.className = "card card-body";
-        div.innerText = JSON.stringify(response);
-        errorContainer.append(div);
-        performerId.value = '';
-        description.value = '';
-        expectedResult.value = '';
-        expectedDueDate.value = '';
-      }
-    ).catch(error => fail(error));
+  sendRequest(
+    'POST',
+    '/api/tasks/',
+    {
+      performerId: $('#add-task_performer').val(),
+      description: $('#add-task_description').val(),
+      expectedResult: $('#add-task_expected_result').val(),
+      expectedDueDate: $('#add-task_expected_due_date').val()
+    }
+  ).then( response => {
+      console.log("addTask::response:", response);
+      location.reload();
+    }
+  ).catch(error => fail(error));
 }
 
-async function postData(url = '', data = {}) {
+async function sendRequest(method = 'GET', url = '', data = {}) {
   const response = await fetch(url, {
-    method: 'POST',
+    method: method,
     mode: 'same-origin',
     credentials: 'same-origin',
     headers: {
@@ -99,10 +95,12 @@ async function postData(url = '', data = {}) {
     },
     body: JSON.stringify(data)
   });
+  if (response.redirected && response.url.match('/login$')) {
+    throw "You are not logged in!";
+  }
   const statusOk = await response.ok;
   if (!statusOk) {
-    const responseText = await response.text();
-    throw new Error(responseText);
+    throw await response.text()
   }
   return await response.json();
 }
